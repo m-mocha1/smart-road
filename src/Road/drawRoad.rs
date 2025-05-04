b7dc46f9a94ea20fe72d39683c519f6c603e08aa
Admare <admaremhamad@gmail.com>
2025-05-04 19:11:49 +0300

    Update drawRoad module to enhance window size and intersection drawing functionality

--- src/Road/drawRoad.rs
+++ src/Road/drawRoad.rs
@@ -11,8 +11,10 @@ pub fn open_window() -> Result<(), String> {
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("Smart Intersection", 800, 600)
-        .position(2500, 250)
+        .window("Smart Intersection", 1000, 1000)
+        // .position(2500, 250)
+        // .position_centered()
+        .position(900, 80)
         .build()
         .map_err(|e| e.to_string())?;
 
@@ -43,10 +45,12 @@ pub fn open_window() -> Result<(), String> {
 pub fn draw_intersection(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
 ) -> Result<(), String> {
-    const SCREEN_SIZE: u32 = 800;
-    const LANE_WIDTH: u32 = 20;
+    const SCREEN_SIZE: u32 = 1000;
+    const LANE_WIDTH: u32 = 47;
     const ROAD_WIDTH: u32 = LANE_WIDTH * 6;
     const MID_WIDTH: i32 = ((SCREEN_SIZE / 2) - 140) as i32;
+
+    let top_lines = (SCREEN_SIZE - 640) as u32;
 
     let mid = (SCREEN_SIZE / 2) as i32;
     let half_road = (ROAD_WIDTH / 2) as i32;
@@ -55,8 +59,10 @@ pub fn draw_intersection(
     canvas.set_draw_color(Color::RGB(30, 30, 30));
     canvas.clear();
 
+    //Road color
+    canvas.set_draw_color(Color::RGB(60, 60, 60));
+
     // Vertical road
-    canvas.set_draw_color(Color::RGB(80, 80, 80));
     canvas.fill_rect(Rect::new(mid - half_road, 0, ROAD_WIDTH, SCREEN_SIZE))?;
 
     // Horizontal road
@@ -64,21 +70,63 @@ pub fn draw_intersection(
 
     // Lane markings - optional: yellow/white lines
     canvas.set_draw_color(Color::RGB(255, 255, 0));
-    for i in 1..3 {
-        // Horizontal markings
+
+    for i in 0..7 {
+        // Vertical markings
         let offset = i * LANE_WIDTH;
+        if i == 0 || i == 6 {
+            canvas.fill_rect(Rect::new(
+                mid - half_road + offset as i32 - 1,
+                0,
+                2,
+                SCREEN_SIZE,
+            ))?;
+        } else if i == 3 {
+            canvas.set_draw_color(Color::RGB(255, 255, 255));
+            canvas.fill_rect(Rect::new(
+                mid - half_road + offset as i32 - 1,
+                0,
+                2,
+                top_lines,
+            ))?;
+        } else {
+            // top lines
+            canvas.set_draw_color(Color::RGB(255, 255, 0));
+            canvas.fill_rect(Rect::new(
+                mid - half_road + offset as i32 - 1,
+                0,
+                2,
+                top_lines,
+            ))?;
+            canvas.fill_rect(Rect::new(
+                mid - half_road + offset as i32 - 1,
+                mid + half_road,
+                2,
+                top_lines,
+            ))?;
+        }
+
+        // Horizontal markings
+        if i == 0 || i == 6 {
+            canvas.fill_rect(Rect::new(
+                0,
+                mid - half_road + offset as i32 - 1,
+                SCREEN_SIZE,
+                2,
+            ))?;
+        }
+
         canvas.fill_rect(Rect::new(
-            mid - half_road + offset as i32 - 1,
             0,
+            mid - half_road + offset as i32 - 1,
+            top_lines,
             2,
-            SCREEN_SIZE,
         ))?;
 
-        // Vertical markings
         canvas.fill_rect(Rect::new(
-            0,
-            mid - half_road + offset as i32 - 1,
-            SCREEN_SIZE,
+            mid + half_road,
+            mid + half_road - offset as i32 + 1,
+            top_lines,
             2,
         ))?;
     }
